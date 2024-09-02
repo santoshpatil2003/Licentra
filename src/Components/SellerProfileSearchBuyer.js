9bb1c51d65de7c9a5c30bff77bc6d3c97dd9febd
santoshpatil2003 <santoshpatil2003@gmail.com>
2024-09-02 14:04:07 +0530

    eighth commit

--- src/Components/SellerProfileSearchBuyer.js
+++ src/Components/SellerProfileSearchBuyer.js
@@ -3,6 +3,9 @@ import {CardContent, Typography, Box, Avatar } from '@mui/joy';
 import images from "./images.jpeg"
 
 
+let l = {2: 'red', 3 : "blue", 4: 'green', 5: 'gray', 6: 'brown', 7: 'pink', 8: 'purple', 9: 'greenyellow', 10: 'blueviolet', 11: 'burlywood',12: 'red', 13 : "blue", 14: 'green', 15: 'gray', 16: 'brown', 17: 'pink', 18: 'purple'}
+
+
 function SellerProfileSearchBuyer({show, Name, UserName, image, GoSellerProfilePage, puid}) {
     const title = (
         <CardContent sx={{display:'flex', justifyContent: 'start', marginLeft: '0%'}}>
@@ -15,13 +18,17 @@ function SellerProfileSearchBuyer({show, Name, UserName, image, GoSellerProfileP
         </CardContent>
     );
 
+    let len = UserName.split('').length
+    let col = l[len]
+
     return (
         show? 
         <div onClick={()=>{GoSellerProfilePage(puid)}} style={{height: '10vh', width: '100%', borderRadius: '10px', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
             <Box height={'100%'} width={'100%'}  sx={{'&:hover': {cursor: 'pointer'}}}>
                 <Box bgcolor={'black'} margin={'1%'} height={'100%'} width={'100%'} borderRadius={10} display={'flex'} justifyContent={'start'} flexDirection={'row'}>
                     <Box height={'100%'} width={'7%'} display={'flex'} justifyContent={'center'} alignItems={'center'} >
-                        <Avatar alt="avatar" src={images} size="lg" />
+                        {/* <Avatar alt="avatar" src={images} size="lg" /> */}
+                        <Box bgcolor={col} height={'75%'} width={'70%'} borderRadius={150} display={'flex'} justifyContent={'center'} alignItems={'center'}><Typography sx={{color: 'white'}}>{Name.split('')[0].toUpperCase()}</Typography></Box>
                     </Box>
                     <Box width={'60%'} display={'flex'} justifyContent={'start'} alignItems={'center'}>
                         <Box width={'90%'} height={'90%'} flexDirection={'column'} display={'flex'} justifyContent={'center'} alignItems={'start'}>
--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -4,22 +4,29 @@ import SideBarButtons from './SideBarButtons';
 import images from "./images.jpeg"
 import {getSeller } from '../Backend/Data';
 
+
+let l = {2: 'red', 3 : "blue", 4: 'green', 5: 'gray', 6: 'brown', 7: 'pink', 8: 'purple', 9: 'greenyellow', 10: 'blueviolet', 11: 'burlywood',12: 'red', 13 : "blue", 14: 'green', 15: 'gray', 16: 'brown', 17: 'pink', 18: 'purple'}
+
 function SideBar({select_click, selected, uid}) {
     const [b_user_data, b_user_data_f] = useState({});
+    const [col, colf] = useState('green');
 
     useEffect(()=>{
         const fetch = async () => {
             let ob = await getSeller(uid)
             b_user_data_f(ob.data)
+            // console.log(b_user_data.name.split('').length)
+            colf(l[ob.data.name.split('').length])
         }
         fetch()
-    },[uid]);
+    },[uid,col]);
     
     return (
         <Box maxWidth={"25vw"} maxHeight={'100vh'} height={"100vh"} bgcolor={'#13121D'} display={'flex'} flexDirection={'column'} flexBasis={"30%"}>
             <Box height={"40vh"} display={'flex'} justifyContent={'center'} alignItems={'end'}>
                 <Box height={"35vh"} width={"35vh"} borderRadius={150} bgcolor={'black'} display={'flex'} justifyContent={'center'} alignItems={'center'}>
-                    <img src={images} alt="Profile" style={{ objectFit: "cover", overflow: "hidden", borderRadius: "200px" }} />
+                    {/* <img src={images} alt="Profile" style={{ objectFit: "cover", overflow: "hidden", borderRadius: "200px" }} /> */}
+                    {b_user_data.name === undefined?<Box bgcolor={col} height={'100%'} width={'100%'} borderRadius={150} display={'flex'} justifyContent={'center'} alignItems={'center'}><Typography sx={{color: 'white', fontSize: '1rem'}}>{'Loading...'}</Typography></Box>: <Box bgcolor={col} height={'100%'} width={'100%'} borderRadius={150} display={'flex'} justifyContent={'center'} alignItems={'center'}><Typography sx={{color: 'white', fontSize: '3rem'}}>{b_user_data.name.split('')[0].toUpperCase()}</Typography></Box>}
                 </Box>
             </Box>
             <Box width={'100%'} height={'fit-content'} display={'flex'} justifyContent={'center'} alignItems={'center'} flexDirection={'column'}>
